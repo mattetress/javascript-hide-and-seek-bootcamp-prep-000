a868ff23eac7a5ff94bc308d94163a1cd63a56de
Matthew Etress <mattetress@me.com>
2018-08-31 20:34:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+function getFirstSelector(selector) {
+  let first = document.querySelector(selector);
+  return first;
+}
 
+function nestedTarget(){
+  let target = document.getElementById("nested").querySelectorAll(".target");
+  return target;
+}
+
+function increaseRankBy(n) {
+  
+}
