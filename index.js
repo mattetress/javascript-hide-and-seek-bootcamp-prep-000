e62b4e905201d80325f7181ddc5af197e6921aab
Matthew Etress <mattetress@me.com>
2018-08-31 21:08:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ function increaseRankBy(n) {
   let lists = document.getElementById('app').querySelectorAll("ul.ranked-list li");
   
   for (let i = 0; i < lists.length; i++) {
-  lists[i].innerHTML = parseInt(lists[i].innerHTML) + n;}
-  return lists;
+  lists[i].innerHTML = parseInt(lists[i].innerHTML) + n;
+  }
+}
+
+function deepestChild() {
+  
 }
